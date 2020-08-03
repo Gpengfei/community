30864ce3548c81ac1df3655ec01a94c98dd01d02
Gpengfei <gpfsjdf@163.com>
2020-08-03 18:03:31 +0800

    Update http.js

--- shequ/src/utils/http.js
+++ shequ/src/utils/http.js
@@ -6,11 +6,12 @@ import axios from 'axios';
 import router from '../router';
 import store from '../store/index';
 import { Message } from 'element-ui';
-
+import qs from 'qs';
 /**
  * 提示函数
  * 禁止点击蒙层、显示一秒后关闭
  */
+const loadingQueue = {};
 const tip = msg => {
     Message(msg);
 }
@@ -55,50 +56,39 @@ const errorHandle = (status, other) => {
             break;
         default:
             console.log(other);
-        }}
+    }
+}
 
 // 创建axios实例
-var instance = axios.create({    timeout: 1000 * 12});
+var instance = axios.create({ timeout: 1000 * 12 });
 // 设置post请求头
 instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 /**
  * 请求拦截器
  * 每次请求前，如果存在token则在请求头中携带token
  */
-instance.interceptors.request.use(
-    config => {
-        // 登录流程控制中，根据本地是否存在token判断用户的登录情况
-        // 但是即使token存在，也有可能token是过期的，所以在每次的请求头中携带token
-        // 后台根据携带的token判断用户的登录情况，并返回给我们对应的状态码
-        // 而后我们可以在响应拦截器中，根据状态码进行一些统一的操作。
-        const token = store.state.tokens;
-        token && (config.headers.Authorization = token);
-        return config;
-    },
-    error => Promise.error(error))
+instance.interceptors.request.use(function(config){
+    config.data = qs.stringify(config.data)
+    if (Object.keys(loadingQueue).length == 0) {
+        store.state.isShowLoading = true  //改变为true
+    }
+    loadingQueue[config.url] = true
+    return config;
+},function(error) {
+    return Promise.reject(error);
+})
 
 // 响应拦截器
-instance.interceptors.response.use(
-    // 请求成功
-    res => res.status === 200 ? Promise.resolve(res) : Promise.reject(res),
-    // 请求失败
-    error => {
-        const { response } = error;
-        if (response) {
-            // 请求已发出，但是不在2xx的范围
-            errorHandle(response.status, response.data.message);
-            return Promise.reject(response);
-        } else {
-            // 处理断网的情况
-            // eg:请求超时或断网时，更新state的network状态
-            // network状态在app.vue中控制着一个全局的断网提示组件的显示隐藏
-            // 关于断网组件中的刷新重新获取数据，会在断网组件中说明
-            if (!window.navigator.onLine) {
-               store.commit('changeNetwork', false);
-            } else {
-                return Promise.reject(error);
-            }
-        }
-    });
+instance.interceptors.response.use(function(response) {
+    /* TODOS 使用store进行loading方法解除 */
+    // console.log('loading结束');
+    delete loadingQueue[response.config.url];  //删除路径 
+    if (Object.keys(loadingQueue).length == 0) {
+        store.state.isShowLoading = false  //改变为false
+    }
+    return response;
+}, function(error) {
+    return Promise.reject(error);
+});
 
 export default instance;
