b2c9fc3d8601ef3b78a94caae42f698a795574c5
Gpengfei <gpfsjdf@163.com>
2020-08-03 11:21:58 +0800

    Update SecondHandGoods.vue

--- shequ/src/views/release/SecondHandGoods.vue
+++ shequ/src/views/release/SecondHandGoods.vue
@@ -3,32 +3,31 @@
     <div class="releaseHome-title">
       <p>二手物品</p>
     </div>
-    <!--服务类别-->
+    <!--物品类别-->
 
     <div class="fwxzlb">
-      <p class="titleFw">服务类别</p>
+      <p class="titleFw">物品类别</p>
       <div class="fwxzlx-box">
         <span class="bt">*</span>
-        <span class="text">房产</span>
-        <el-radio v-model="radio" label="1">保洁劳务</el-radio>
-        <el-radio v-model="radio" label="2">开锁/换锁/修锁</el-radio>
-        <el-radio v-model="radio" label="3">管道疏通</el-radio>
+        <span class="text">物品类别</span>
+        <el-radio v-model="radio" label="1">互换</el-radio>
+        <el-radio v-model="radio" label="2">寻物</el-radio>
       </div>
     </div>
     <!--服务基本信息-->
 
     <div class="fwjbxx">
-      <p class="titleFw">基本信息</p>
+      <p class="titleFw">物品信息</p>
       <div class="fwjbxx-box">
         <div class="fwjbxx-lis">
           <span class="bt">*</span>
           <span class="text">标题</span>
-          <input type="text" placeholder="请输入标题" class="inp" />
+          <input type="text" placeholder="请输入物品标题" class="inp" />
         </div>
         <div class="fwjbxx-lis">
           <span class="bt">*</span>
           <span class="text">服务区域</span>
-          <el-select v-model="value1" multiple placeholder="请选择">
+          <el-select v-model="value1" multiple placeholder="请选择服务区域最多选择2个，多个默认选择前两个">
             <el-option
               v-for="item in options"
               :key="item.value"
@@ -39,30 +38,23 @@
         </div>
       </div>
     </div>
-    <!--服务特色描述-->
+    <!--物品特色描述-->
     <div class="fwjbxx">
-      <p class="titleFw">服务特色描述</p>
+      <p class="titleFw">物品特色描述</p>
       <div class="fwjbxx-box">
         <div class="fwjbxx-lis">
           <span class="bt">*</span>
           <span class="text">详情描述</span>
           <div class="dxk">
-            <textarea
-              name
-              id
-              cols="30"
-              rows="10"
-              placeholder="案例文本：
-服务特色：地毯清洁、开荒保洁、物业保洁
-服务描述：专业承接新居、旧居、楼宇等全面日常保洁、开荒保洁、地毯清洗等工作。"
-            ></textarea>
+            <textarea name id cols="30" rows="10" placeholder="案例文本：
+品牌型号、新旧程度、入手渠道、转手原因"></textarea>
           </div>
         </div>
       </div>
     </div>
     <!--服务图片-->
     <div class="fwjbxx">
-      <p class="titleFw">服务图片</p>
+      <p class="titleFw">物品图片</p>
       <div class="fwjbxx-box">
         <div class="fwjbxx-lis">
           <span class="bt">*</span>
@@ -86,24 +78,8 @@
     </div>
     <!--联系商家-->
     <div class="fwjbxx" style="border-bottom: none;">
-      <p class="titleFw">联系商家</p>
+      <p class="titleFw">联系人信息</p>
       <div class="fwjbxx-box">
-        <div class="fwjbxx-lis">
-          <span class="bt">*</span>
-          <span class="text">服务区域</span>
-          <el-cascader
-            placeholder="请选择地区"
-            popper-class="popper1"
-            :options="options1"
-            v-model="selectedOptions"
-            @change="handleChange"
-          ></el-cascader>
-        </div>
-        <div class="fwjbxx-lis">
-          <span class="bt">*</span>
-          <span class="text">详细地址</span>
-          <input type="text" placeholder="请输入标题" class="inp" />
-        </div>
         <div class="fwjbxx-lis">
           <span class="bt">*</span>
           <span class="text">联系姓名</span>
@@ -127,7 +103,33 @@
 import "@style/release/secondHandGoods.scss";
 export default {
   data() {
-    return {};
+    return {
+      radio: "1",
+      //   选择服务区域
+      options: [
+        {
+          value: "选项1",
+          label: "黄金糕",
+        },
+        {
+          value: "选项2",
+          label: "双皮奶",
+        },
+        {
+          value: "选项3",
+          label: "蚵仔煎",
+        },
+        {
+          value: "选项4",
+          label: "龙须面",
+        },
+        {
+          value: "选项5",
+          label: "北京烤鸭",
+        },
+      ],
+      value1: [],
+    };
   },
   mounted() {
     this.$store.dispatch("setNavFb", 1);
