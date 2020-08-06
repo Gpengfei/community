77b5d6cb17d516fccb4e74a7321a271bb2883a25
Gpengfei <gpfsjdf@163.com>
2020-08-06 10:45:43 +0800

    Update Map.vue

--- shequ/src/components/Map.vue
+++ shequ/src/components/Map.vue
@@ -13,20 +13,21 @@ export default {
     return {};
   },
   methods: {
-    init() {
+    init(longitude, latitude) {
       let map = new AMap.Map("map", {
         zoom: 16, //级别
-        center: [116.397428, 39.90923], //中心点坐标
+        center: [longitude, latitude], //中心点坐标
         viewMode: "3D", //使用3D视图
       });
       var marker = new AMap.Marker({
-        position: [116.397428, 39.90923],
+        position: [longitude, latitude],
       });
       map.add(marker);
     },
   },
   mounted() {
-    this.init();
+    console.log(this.longitude, this.latitude);
+    this.init(this.longitude, this.latitude);
   },
 };
 </script>
